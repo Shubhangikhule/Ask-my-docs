a7bb7816f88cd4da99882d21d137c9813163bf94
Shubhangi Khule <shubhangikhule25@gmail.com>
2026-07-28 19:14:47 +0530

    Enable upload router

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from config import settings
 
 from routers.health import router as health_router
-#from routers.upload import router as upload_router
+from routers.upload import router as upload_router
 #from routers.search import router as search_router
 #from routers.chat import router as chat_router
 
@@ -24,6 +24,6 @@ app.add_middleware(
 )
 
 app.include_router(health_router)
-#app.include_router(upload_router)
+app.include_router(upload_router)
 #app.include_router(search_router)
 #app.include_router(chat_router)
